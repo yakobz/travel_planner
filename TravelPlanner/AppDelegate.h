022800d2efcb34df21aebfbf5c97bcb178a3dd96
Yakov <yayakobz@gmail.com>
2015-05-12 13:55:17 +0300

    Initial Commit

---
+++ TravelPlanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravelPlanner
+//
+//  Created by Yakov on 5/12/15.
+//  Copyright (c) 2015 yakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
